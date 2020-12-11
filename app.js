7a92414e182204ed3ff30a76bae3e51db2e27684
torianyap <torianyap@gmail.com>
2020-12-11 11:46:38 +0700

    add routes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const routes = require('./routes/index')
 
 const app = express()
 const PORT = 3000
 
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(routes)
 
-app.listen(PORT, () => console.log('listening at port' + PORT))
+app.listen(PORT, () => console.log('listening at port', PORT))
---
+++ controllers/favorite.js
@@ -0,0 +1,3 @@
+const { db } = require('../config/mongo')
+
+const users = db.collection('Favorites')
