caa79702ef0fb2587377ed40ec02cb5225829e9a
abrarnaim015 <aabrarnaim24@gmail.com>
2020-12-15 19:39:18 +0700

    Add Google Sign In

--- routes/user_route.js
+++ routes/user_route.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/users');
 
 route.post('/register', UserController.register);
 route.post('/login', UserController.login);
+route.post('/googlesignin', UserController.googleSignIn)
 
 module.exports = route
